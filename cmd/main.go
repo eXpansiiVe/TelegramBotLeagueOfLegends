1f706ed00fbf7227a3052bbf82c5f85b2b0f786d
eXpansiiVe <sergio120800@gmail.com>
2022-11-23 00:34:57 +0100

    Addess sendmessage from the bot

--- cmd/main.go
+++ cmd/main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io"
+	"log"
 	"net/http"
 	"net/url"
 	"time"
@@ -55,18 +57,21 @@ func getRankData(id string) []byte {
 	return responsePlayerData
 }
 
-func filterRankData(schemeLoLData schemes.LoLAccount) string {
+func filterRankData(schemeLoLData schemes.LoLAccount) (string, error) {
 	var playerRanks string = ""
 	var schemeLen int = len(schemeLoLData)
 
 	for i := 0; i < schemeLen; i++ {
 		if schemeLoLData[i].QueueType == "RANKED_SOLO_5x5" {
 			playerRanks += fmt.Sprintf("Rank SoloQ: %s %s\n", schemeLoLData[i].Tier, schemeLoLData[i].Rank)
+			return playerRanks, nil
 		} else {
 			playerRanks += fmt.Sprintf("Rank Flex: %s %s\n", schemeLoLData[i].Tier, schemeLoLData[i].Rank)
+			return playerRanks, nil
 		}
 	}
-	return playerRanks
+	errore := errors.New("no rank found")
+	return "", errore
 }
 
 func getTelegramApi() []byte {
@@ -77,10 +82,33 @@ func getTelegramApi() []byte {
 	return responseTelegramData
 }
 
+func sendHttpMessage(chatId int64, messageId int, message string) ([]byte, error) {
+	// Debug
+	fmt.Println(chatId)
+	fmt.Println(message)
+	fmt.Println(messageId)
+
+	chatIdString := fmt.Sprintf("%d", chatId)
+	messageIdString := fmt.Sprintf("%d", messageId)
+	formattedUrl := "https://api.telegram.org/bot5683492318:AAFW8Yt40ggMfd7eP5p-Ea1pzao2G_oAgsg/sendMessage?chat_id=" + url.QueryEscape(chatIdString) + "&reply_to_message_id=" + url.QueryEscape(messageIdString) + "&text=" + url.QueryEscape(message)
+	fmt.Println(formattedUrl)
+	rawResponseData, err := http.Get(formattedUrl)
+	if err != nil {
+		return nil, err
+	}
+	defer rawResponseData.Body.Close()
+	responseData, err := io.ReadAll(rawResponseData.Body)
+	if err != nil {
+		return nil, err
+	}
+	return responseData, nil
+}
+
 func main() {
 	var schemeTg schemes.ApiTelegram
 	var schemeAccount schemes.AccountRiot
 	var schemeLoLData schemes.LoLAccount
+	var schemeTgMessageResponse schemes.ApiTelegramMessage
 
 	var updateID int
 
@@ -93,6 +121,7 @@ func main() {
 		time.Sleep(5 * time.Second)
 
 		if updateID == schemeTg.Result[0].Message.MessageID {
+			fmt.Println("Inside if")
 			continue
 		}
 		responseAccountData := getAccountID(schemeTg.Result[0].Message.Text)
@@ -101,9 +130,20 @@ func main() {
 		rankData := getRankData(schemeAccount.ID)
 		json.Unmarshal(rankData, &schemeLoLData)
 
-		rank := filterRankData(schemeLoLData)
+		rank, err := filterRankData(schemeLoLData)
+		message := fmt.Sprintf("%s\n%s", schemeTg.Result[0].Message.Text, rank)
+		if err != nil {
+			log.Println(err)
+			message = fmt.Sprintf("No rank found for %s", schemeTg.Result[0].Message.Text)
+		}
 
-		fmt.Printf("%s\n%s", schemeTg.Result[0].Message.Text, rank)
+		
+		responseMessage, err := sendHttpMessage(schemeTg.Result[0].Message.Chat.ID, schemeTg.Result[0].Message.MessageID, message)
+		if err != nil {
+			log.Println(err)
+		}
+		json.Unmarshal(responseMessage, &schemeTgMessageResponse)
+		fmt.Println(schemeTgMessageResponse.Result)
 		updateID = schemeTg.Result[0].Message.MessageID
 
 	}
