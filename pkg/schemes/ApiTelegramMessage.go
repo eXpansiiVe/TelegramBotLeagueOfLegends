a94412852d28884fad3dd825e35fe66531e48b42
eXpansiiVe <sergio120800@gmail.com>
2022-11-23 00:35:07 +0100

    New struct added

---
+++ pkg/schemes/ApiTelegramMessage.go
@@ -0,0 +1,21 @@
+package schemes
+
+type ApiTelegramMessage struct {
+	Ok     bool `json:"ok"`
+	Result struct {
+		MessageID int `json:"message_id"`
+		From      struct {
+			ID        int64  `json:"id"`
+			IsBot     bool   `json:"is_bot"`
+			FirstName string `json:"first_name"`
+			Username  string `json:"username"`
+		} `json:"from"`
+		Chat struct {
+			ID    int64  `json:"id"`
+			Title string `json:"title"`
+			Type  string `json:"type"`
+		} `json:"chat"`
+		Date int    `json:"date"`
+		Text string `json:"text"`
+	} `json:"result"`
+}
