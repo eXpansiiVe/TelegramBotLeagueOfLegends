0803eb4955085f9b6f42f1492b320b4312280789
eXpansiiVe <sergio120800@gmail.com>
2022-11-26 16:44:22 +0100

    New struct file

---
+++ pkg/schemes/LoLAccountData.go
@@ -0,0 +1,14 @@
+package schemes
+
+type LoLAccountData []struct {
+	SummonerName string
+	Level        int
+	RankSoloq    string
+	RankFlex     string
+	LpSoloq      int
+	LpFlex       int
+	WinsSoloq    int
+	WinsFlex     int
+	LossesSoloq  int
+	LossesFlex   int
+}
